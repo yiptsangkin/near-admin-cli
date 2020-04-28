6111d308aaf0b4bc7551e92d7e9b1e7aa44693a5
nearyip <oncwnuG4NQVZAopl_epdjVD6GPjU@git.weixin.qq.com>
2020-04-28 15:41:09 +0800

    feat(修改commitlint配置): 修改commitlint配置，增加中文的类型

--- commitlint.config.js
+++ commitlint.config.js
@@ -3,6 +3,48 @@ module.exports = {
     '@commitlint/config-conventional'
   ],
   rules: {
-    'type-enum': [2, 'always', ['新增', '更新']]
+    'body-leading-blank': [1, 'always'],
+    'footer-leading-blank': [1, 'always'],
+    'header-max-length': [2, 'always', 72],
+    'scope-case': [2, 'always', 'lower-case'],
+    'subject-case': [
+      2,
+      'never',
+      ['sentence-case', 'start-case', 'pascal-case', 'upper-case']
+    ],
+    'subject-empty': [2, 'never'],
+    'subject-full-stop': [2, 'never', '.'],
+    'type-case': [2, 'always', 'lower-case'],
+    'type-empty': [2, 'never'],
+    'type-enum': [
+      2,
+      'always',
+      [
+        'build',
+        'chore',
+        'ci',
+        'docs',
+        'feat',
+        'fix',
+        'improvement',
+        'perf',
+        'refactor',
+        'revert',
+        'style',
+        'test',
+        '构建',
+        '日常维护',
+        '持续集成',
+        '文档',
+        '新功能',
+        '修复',
+        '改善',
+        '性能',
+        '重构',
+        '回滚',
+        '样式',
+        '测试'
+      ]
+    ]
   }
 }
